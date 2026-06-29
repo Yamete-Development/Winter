e493b91766edcceba543eb2f2869b64e3b82ff8f
Chanakan Moongthin <me@chanakanlabs.com>
2026-06-29 15:55:49 +0700

    fix: rename REDIS_URL to REDIS_URI

--- app/redis.server.ts
+++ app/redis.server.ts
@@ -10,10 +10,10 @@ declare global {
 // the server with every change, but we want to make sure we don't
 // create a new connection to the Redis with every change either.
 if (process.env.NODE_ENV === "production") {
-  redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+  redis = new Redis(process.env.REDIS_URI || "redis://localhost:6379");
 } else {
   if (!global.__redis) {
-    global.__redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+    global.__redis = new Redis(process.env.REDIS_URI || "redis://localhost:6379");
   }
   redis = global.__redis;
 }
