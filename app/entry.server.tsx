37efd6bfdc4ce791d6ef6f99966f8f06c2ef005f
dev-737 <73829355+dev-737@users.noreply.github.com>
2026-06-30 10:29:37 +0530

    refactor: upgrade react router to v8

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,28 +1,25 @@
 import { isbot } from "isbot";
 import { renderToReadableStream } from "react-dom/server";
-import type { AppLoadContext, EntryContext } from "react-router";
+import type { EntryContext } from "react-router";
 import { ServerRouter } from "react-router";
 
 export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  routerContext: EntryContext,
-  loadContext: AppLoadContext
+  routerContext: EntryContext
 ) {
   const body = await renderToReadableStream(
     <ServerRouter context={routerContext} url={request.url} />,
     {
       signal: request.signal,
       onError(error: unknown) {
-        // Log stream errors to the console
         console.error(error);
         responseStatusCode = 500;
       },
     }
   );
 
-  // If the request is from a bot (like Google), wait for the entire stream to finish before responding
   if (isbot(request.headers.get("user-agent") || "")) {
     await body.allReady;
   }
